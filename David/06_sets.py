b50a6e69fd0d30e05f90b37c7c949244a34df827
davld7 <118039588+davld7@users.noreply.github.com>
2023-01-07 22:40:07 -0600

    Ejemplos de Sets.

---
+++ David/06_sets.py
@@ -0,0 +1,11 @@
+## Sets ##
+
+my_set = set()
+my_other_set = {} #Inicialmente un diccionario.
+
+print(type(my_set))
+print(type(my_other_set))
+
+my_other_set = {25, "David", "Ramos"}
+print(my_other_set)
+print(type(my_other_set))
