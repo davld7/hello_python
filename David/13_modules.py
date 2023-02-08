55473643edb203e58f35fc9a2eefab80411b6faf
David Ramos <komorebi.david@outlook.com>
2023-02-07 23:07:52 -0600

    Ejemplos de Modules.

---
+++ David/13_modules.py
@@ -0,0 +1,17 @@
+## Modules ##
+
+from math import pi as pi_value
+import math
+
+from my_module import sum_value, print_value
+import my_module
+
+my_module.sum_value(5, 1, 1)
+my_module.print_value("Hola Python.")
+
+sum_value(5, 1, 1)
+print_value("Hola Python.")
+
+print(math.pi)
+print(math.pow(2, 8))
+print(pi_value)
