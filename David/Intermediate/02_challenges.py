b57046a0ac04354f945bb1ed501fc692bfe1dad4
David Ramos <komorebi.david@outlook.com>
2023-03-07 00:38:03 -0600

    Retos de Programación.

---
+++ David/Intermediate/02_challenges.py
@@ -0,0 +1,44 @@
+## Challenges ##
+
+"""
+Reto #0: EL FAMOSO "FIZZ BUZZ”
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+"""
+
+def fizzbuzz():
+    for index in range(1, 101):
+        if index % 3 == 0 and index % 5 == 0:
+            print("fizzbuzz")
+        elif index % 3 == 0:
+            print("fizz")
+        elif index % 5 == 0:
+            print("buzz")
+        else:
+            print(index)
+fizzbuzz()
+
+"""
+/*
+Reto #1: ¿ES UN ANAGRAMA?
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+"""
+
+def is_anagram(word_one, word_two):
+    if word_one.lower() == word_two.lower():
+         return False
+    return sorted(word_one.lower()) == sorted(word_two.lower())
+
+print(is_anagram("Amor", "Roma"))
